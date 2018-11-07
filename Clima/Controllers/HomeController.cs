48cf45eb9942c1c165b7023e1cb1132119594a97
Ricardo <rialf.rodriguez@gmail.com>
2018-11-07 12:04:05 -0300

    primer commit

---
+++ Clima/Controllers/HomeController.cs
@@ -0,0 +1,39 @@
+﻿using Clima.Models.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Clima.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index(Country model)
+        {
+            List<City> cities = new List<City>() {
+                new City(){Id = 1, CityName="Hyderabad"},
+                new City(){Id = 2, CityName="Ongole"},
+                new City(){Id = 3, CityName="Guntur"},
+                new City(){Id = 4, CityName="Vijayawada"}
+            };
+
+            model.Cities = cities;
+            return View(model);
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
---
+++ Clima/Models/ViewModels/Encuesta.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Clima.Models.ViewModels
+{
+    public class Encuesta
+    {
+        public int IdEncuesta { get; set; }
+        public short IdPeriodo { get; set; }
+        public string Nombre { get; set; }
+        public string Descripcion { get; set; }
+        public short Activo { get; set; }
+    }
+}
